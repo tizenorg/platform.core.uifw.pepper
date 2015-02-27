b3cee970a395dfffac86a506befd8ecc53995f36
Taekyun Kim <tkq.kim@samsung.com>
2015-02-27 13:18:58 +0900

    Modifled pepper.c to apply coding style guide line
    Change-Id: Ie9b19eeb5b191f0770875e3fe257dc3f7fd7f6ef

--- src/pepper.c
+++ src/pepper.c
@@ -6,38 +6,38 @@
 int
 main(int argc, char **argv)
 {
-	int			ret = EXIT_SUCCESS;
-	struct wl_display *	display = NULL;
-	const char *		socket_name = NULL;
+    int			ret = EXIT_SUCCESS;
+    struct wl_display *	display = NULL;
+    const char *		socket_name = NULL;
 
-	/* Create a display object. */
-	display = wl_display_create();
+    /* Create a display object. */
+    display = wl_display_create();
 
-	if (!display)
-	{
-		printf("Failed to create display.\n");
-		ret = EXIT_FAILURE;
-		goto out;
-	}
+    if (!display)
+    {
+	printf("Failed to create display.\n");
+	ret = EXIT_FAILURE;
+	goto out;
+    }
 
-	/* Add a listening socket. */
-	socket_name = wl_display_add_socket_auto(display);
+    /* Add a listening socket. */
+    socket_name = wl_display_add_socket_auto(display);
 
-	if (!socket_name)
-	{
-		printf("Failed to add socket.\n");
-		ret = EXIT_FAILURE;
-		goto out;
-	}
+    if (!socket_name)
+    {
+	printf("Failed to add socket.\n");
+	ret = EXIT_FAILURE;
+	goto out;
+    }
 
-	printf("Pepper server socket added: %s\n", socket_name);
+    printf("Pepper server socket added: %s\n", socket_name);
 
-	/* Start main loop. */
-	wl_display_run(display);
+    /* Start main loop. */
+    wl_display_run(display);
 
 out:
-	if (display)
-		wl_display_destroy(display);
+    if (display)
+	wl_display_destroy(display);
 
-	return ret;
+    return ret;
 }
