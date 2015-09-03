c8a9eeffee46e94d866bba9f1d88c34ec41bdaf2
Taekyun Kim <tkq.kim@samsung.com>
2015-09-03 17:31:57 +0900

    samples: x11-backend: Handle SIGINT and free resources when finished
    Change-Id: Ie60a229b4117c0e806c165b1b966a3cea84d0bab

--- src/samples/x11-backend.c
+++ src/samples/x11-backend.c
@@ -1,6 +1,16 @@
 #include <pepper.h>
 #include <pepper-x11.h>
 #include <pepper-desktop-shell.h>
+#include <signal.h>
+
+static int
+handle_sigint(int signal_number, void *data)
+{
+    struct wl_display *display = (struct wl_display *)data;
+    wl_display_terminate(display);
+
+    return 0;
+}
 
 int
 main(int argc, char **argv)
@@ -9,6 +19,8 @@ main(int argc, char **argv)
     pepper_output_t         *output;
     pepper_output_mode_t     mode;
     pepper_x11_connection_t *conn;
+    struct wl_event_loop    *loop = NULL;
+    struct wl_event_source  *sigint = NULL;
 
     struct wl_display       *display;
 
@@ -35,7 +47,14 @@ main(int argc, char **argv)
     display = pepper_compositor_get_display(compositor);
     PEPPER_ASSERT(display);
 
+    loop = wl_display_get_event_loop(display);
+    sigint = wl_event_loop_add_signal(loop, SIGINT, handle_sigint, display);
+
     wl_display_run(display);
+
+    wl_event_source_remove(sigint);
+    pepper_x11_destroy(conn);
+    pepper_compositor_destroy(compositor);
 
     return 0;
 }
